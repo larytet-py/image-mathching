d4a88b22df4ef2ffa5094883146d562ca355a1aa
arkady <larytet@yahoo.com>
2019-09-16 14:15:17 +0300

    Compare works

--- color-palette.py
+++ color-palette.py
@@ -17,6 +17,10 @@ Options:
 '''
 
 # Based on https://stackoverflow.com/questions/18801218/build-a-color-palette-from-image-url
+# See also https://stackoverflow.com/questions/75891/algorithm-for-finding-similar-images/83486#83486 - lot of tips here 
+# https://stackoverflow.com/questions/1704793/find-images-with-similar-color-palette-with-python
+# https://github.com/myint/perceptualdiff
+# https://stackoverflow.com/questions/5392061/algorithm-to-check-similarity-of-colors - distance between colors
 
 import numpy
 from PIL import Image
